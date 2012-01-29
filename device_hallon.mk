14963e3cb7538f8ff4de1aceedfac9215bdd25ba
freexperia <jerpelea@gmail.com>
2012-01-29 08:09:25 +0200

    semc: kernel update and cleanup

--- device_hallon.mk
+++ device_hallon.mk
@@ -11,16 +11,7 @@ PRODUCT_NAME := hallon
 PRODUCT_DEVICE := hallon
 PRODUCT_MODEL := hallon
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/semc/hallon/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 -include device/semc/mogami-common/mogami.mk
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
 
 # These is the hardware-specific overlay, which points to the location
 # of hardware-specific resource overrides, typically the frameworks and
@@ -28,15 +19,17 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += device/semc/hallon/overlay
 
 # These are the hardware-specific configuration files
-PRODUCT_COPY_FILES += \
-	device/semc/hallon/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+#PRODUCT_COPY_FILES += \
+#	device/semc/hallon/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
 
 # Init files
 PRODUCT_COPY_FILES += \
-    device/semc/hallon/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
     device/semc/hallon/prebuilt/bootrec:root/sbin/bootrec \
-    device/semc/hallon/recovery.fstab:root/recovery.fstab 
+    device/semc/hallon/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+    device/semc/hallon/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
+    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
+    device/semc/hallon/recovery.fstab:root/recovery.fstab
+
 
 # semc msm7x30 uses high-density artwork where available
 PRODUCT_LOCALES += hdpi
@@ -49,33 +42,8 @@ PRODUCT_COPY_FILES += \
     device/semc/msm7x30-common/prebuilt/animations/charging_animation_04_H.png:system/semc/chargemon/data/charging_animation_04.png \
     device/semc/msm7x30-common/prebuilt/animations/charging_animation_05_H.png:system/semc/chargemon/data/charging_animation_05.png \
     device/semc/msm7x30-common/prebuilt/animations/charging_animation_06_H.png:system/semc/chargemon/data/charging_animation_06.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_H.png:system/semc/chargemon/data/charging_animation_07.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_blank_H.png:system/semc/chargemon/data/charging_animation_blank.png
+    device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_H.png:system/semc/chargemon/data/charging_animation_07.png
 
+#LCD DENSITY
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/lib/libril-qc-1.so \
-    rild.libargs=-d/dev/smd0 \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    ro.telephony.default_network=0 \
-    ro.telephony.call_ring.multiple=false \
-    ro.telephony.ril_class=semc \
-    wifi.interface=tiwlan0 \
-    wifi.supplicant_scan_interval=15 \
-    ro.sf.lcd_density=240 \
-    keyguard.no_require_sim=true \
-    ro.com.google.locationfeatures=1 \
-    dalvik.vm.dexopt-flags=m=y \
-    dalvik.vm.heapsize=48m \
-    dalvik.vm.dexopt-data-only=1 \
-    dalvik.vm.lockprof.threshold=500 \
-    dalvik.vm.execution-mode=int:jit \
-    dalvik.vm.checkjni=false \
-    ro.opengles.version=131072  \
-    ro.compcache.default=0 \
-    ro.product.locale.language=en \
-    ro.product.locale.region=US \
-    persist.ro.ril.sms_sync_sending=1 \
-    ro.use_data_netmgrd=true \
-    wifi.hotspot.ti=1 \
-    BUILD_UTC_DATE=0
+    ro.sf.lcd_density=240
--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 VENDOR=semc
-DEVICE=hallon
+DEVICE=urushi
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
