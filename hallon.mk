25ca31137a2cf46bb9672b9a1ac87ff73e6ab78e
freexperia <jerpelea@gmail.com>
2012-08-19 10:25:15 +0300

    hallon: add device configs

--- hallon.mk
+++ hallon.mk
@@ -28,6 +28,17 @@ PRODUCT_COPY_FILES += \
     device/semc/hallon/prebuilt/bootrec-device:root/sbin/bootrec-device \
     device/semc/hallon/recovery.fstab:root/recovery.fstab
 
+# Device specific configs
+PRODUCT_COPY_FILES += \
+    device/semc/hallon/config/cy8ctma300_touch.idc:system/usr/idc/cy8ctma300_touch.idc \
+    device/semc/hallon/config/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
+    device/semc/hallon/config/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
+    device/semc/hallon/config/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
+    device/semc/hallon/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+    device/semc/hallon/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    device/semc/hallon/config/sensors.conf:system/etc/sensors.conf \
+    device/semc/hallon/config/tiwlan.ini:system/etc/tiwlan.ini
+
 $(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
 
 #LCD DENSITY
