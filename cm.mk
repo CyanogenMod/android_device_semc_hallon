df6a5ac7113f6274290cf741e657e0a55cb88346
freexperia <jerpelea@gmail.com>
2012-09-01 08:49:17 +0300

    hallon: bootanimation

--- cm.mk
+++ cm.mk
@@ -10,7 +10,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MT15i BUILD_FINGERPRINT="SEMC/MT15i_1254-2184/MT15i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="MT15i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-480x854
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_hallon
